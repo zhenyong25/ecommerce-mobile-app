b07263f9b642795a360cd8e3f73ad42f361d570d
zhenyong25 <zhenyong25@gmail.com>
2023-07-13 21:34:52 +0800

    Setting Up Application Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants/global_variables.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,18 +14,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+          )),
+      home: Scaffold(
+        // Top Bar
+        appBar: AppBar(
+          title: const Text('Hello.'),
+        ),
+
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Flutter Demo Home Page',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click'),
+            )
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
